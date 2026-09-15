c78ec9b59a4e32e49dbb45f35bce3a7ce880f44e
Julia-48 <juliasoares230897@gmail.com>
2026-09-15 19:04:51 +0000

    aula_15_09

--- aulas/structs.c
+++ aulas/structs.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float trapezio(float (*f)(float), float a, float b, int n){
+    float x, h, area;
+    int i;
+    h = (b - a) / n;
+    area = 0;
+    x = a + h;
+    for(i = 1; i < n; i++){
+        area = area + f(x);
+        x = x + h;
+    }
+    area = area + (f(a) + f(b)) / 2;
+    area = area * h;
+    return area;
+}
 
+float func(float x){ return exp(x);}
+
+int main(void){
+    float a = 0, b = 1;
+    int n = 1000;
+    float area = trapezio(func, a, b, n);
+    printf("area = %f\n", area);
+    return 0;
+}
+
+
+
+
+
+
+
+/*int funcao(int a){
+    return a+1;
+}
+
+int alo(int (*f)(int)){
+    printf("ret = %d\n", f(3));
+}
+
+int main(void){
+    alo(funcao);
+}
+                                                             
 struct Alo {
     int a;
     int b;
@@ -19,3 +61,4 @@ int main(){
     
     return 0;
 }
+*/
